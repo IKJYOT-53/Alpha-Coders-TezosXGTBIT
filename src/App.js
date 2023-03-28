df0bebb41c7356831761aa4baee54bcc3b8c3495
IKJYOT-53 <ikjyot.cse1@gmail.com>
2023-03-28 17:34:10 +0530

    added styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Navbar from "./components/Navbar";
 import Avatar,{genConfig} from "react-nice-avatar";
 import { buyTicketOperation, endGameOperation } from "./utils/operation";
@@ -12,6 +12,11 @@ const App = () => {
   const [tickets, setTickets] = useState(5);
   const [loading, setLoading] = useState(false);
   const [config,setConfig] = useState(null);
+  const [userSelect,setUserSelect] = useState({hair:0,color:0})
+  const [hair,setHair] = useState(null);
+  const [glasses,setGlasses] = useState(null);
+  const [gradient,setGradient] = useState(null);
+  const hairInput = useRef(null);
   // Set players and tickets remaining
   useEffect(() => {
     // TODO 9 - Fetch players and tickets remaining from storage
@@ -45,8 +50,54 @@ const App = () => {
     }
     setLoading(false);
   };
+  const changeHair = () => {
+    
+  }
+  const handleHair = () => {
+    if(hair === 1){
+      setHair(0)
+    }
+    else{
+      setHair(1);
+    }
+  }
+  const handleGlasses = () => {
+    if(glasses === 1){
+      setGlasses(0);
+    }else{
+      setGlasses(1);
+    }
+  }
+  const handleGradient = () => {
+    if(gradient === 1){
+      setGradient(0)
+    }else{
+      setGradient(1)
+    }
+  }
   const changeAvatar = () => {
-    setConfig(genConfig())
+    if(hair === 1 && glasses === 1 && gradient === 1){
+      setConfig(genConfig({ hairStyle: "mohawk" ,glassesStyle:"round",isGradient:true}));
+    }
+    if(hair === 1){
+      setConfig(genConfig({hairStyle:"mohawk"}))
+    }
+    if(hair === 1 && glasses === 1){
+      setConfig(genConfig({hairStyle:"mohawk",glassesStyle:"round"}))
+    }
+    if(hair === 1 && gradient === 1){
+      setConfig(genConfig({hairStyle:"mohawk",isGradient:true}))
+    }
+    if(glasses === 1){
+      console.log("Glasses")
+      setConfig(genConfig({glassesStyle:"round"}))
+    }
+    if(gradient === 1){
+      setConfig(genConfig({isGradient:true}))
+    }
+    if(glasses === 1 && gradient === 1){
+      setConfig(genConfig({isGradient:true,glassesStyle:"round"}))
+    }
     console.log(config)
   }
   // Download Avatar
@@ -93,15 +144,22 @@ const App = () => {
         {config ? 
         <Avatar style={{width:"20rem",height:"20rem"}} {...config} id="avatar"/>
        : <img src="https://cdn.pixabay.com/animation/2022/10/11/03/16/03-16-39-160_512.gif" />}
+
+<div class="form-check form-switch">
+  <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" onChange={handleHair}/>
+  <label class="form-check-label" for="flexSwitchCheckDefault">😊</label>
+</div>
+<div class="form-check form-switch">
+  <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" onChange={handleGlasses}/>
+  <label class="form-check-label" for="flexSwitchCheckDefault">🕶️</label>
+</div>
+<div class="form-check form-switch">
+  <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" onChange={handleGradient}/>
+  <label class="form-check-label" for="flexSwitchCheckDefault">🟢🔵🔴</label>
+</div>
         <button className="btn btn-success btn-lg" onClick={changeAvatar}>Randomize</button>
         <button className="btn btn-danger btn-lg" onClick={downloadAvatar}>Download</button>
-        <div className="mt-2">
-          {players.map((player, index) => (
-            <div key={index}>
-              <b>Ticket {index + 1}:</b> {player}
-            </div>
-          ))}
-        </div>
+
       </div>
     </div>
   );
